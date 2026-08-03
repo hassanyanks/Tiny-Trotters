d5d3b02d6cca91a420f908bb028f2ca3627cb5e7
unknown <hassan.yanks@gmail.com>
2026-08-03 15:01:09 -0500

    modifying function startHttpsServer() with this:     const options = process.env.NODE_ENV === 'dev' ? {                                               key: readFileSync(path.join(__dirname, '../samsKey.key')),                                               cert: readFileSync(path.join(__dirname, '../samsCertificate.crt')),                                               rejectUnauthorized: false,                                             }                                           : {};

--- ponies/bin/httpsServer.js
+++ ponies/bin/httpsServer.js
@@ -4,17 +4,21 @@ import { readFileSync } from 'node:fs';
 import path from 'path';
 import 'dotenv/config';
 
-const PORT = process.env.NODE_ENV ? process.env.DEFAULT_PORT : process.env.DEV_PORT;
-const HOST = process.env.NODE_ENV ? process.env.PROD_HOST : process.env.DEV_HOST;
+const PORT = process.env.NODE_ENV === 'production' ? process.env.DEFAULT_PORT : process.env.DEV_PORT;
+const HOST = process.env.NODE_ENV === 'production' ? process.env.PROD_HOST : process.env.DEV_HOST;
 process.on('uncaughtException', (err) => {
   console.error('Uncaught Exception:', err);
 });
 
 export async function startHttpsServer() {
   try {
-    const options = {
-      rejectUnauthorized: false,
-    };
+    const __dirname = import.meta.dirname;
+    const options = process.env.NODE_ENV === 'dev' ? {
+                                              key: readFileSync(path.join(__dirname, '../samsKey.key')),
+                                              cert: readFileSync(path.join(__dirname, '../samsCertificate.crt')),
+                                              rejectUnauthorized: false,
+                                            } 
+                                          : {};
     const https = await import ('node:https');
     console.log('starting server...')
     https.createServer(options, app, (req, res) => {
