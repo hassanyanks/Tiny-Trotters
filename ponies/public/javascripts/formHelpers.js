d7dc5e4ea402b39014063aa22500ab888fd6a93b
unknown <hassan.yanks@gmail.com>
2026-08-06 09:40:28 -0500

    adding more form event listeners

--- ponies/public/javascripts/formHelpers.js
+++ ponies/public/javascripts/formHelpers.js
@@ -3,29 +3,61 @@ var accessoriesSelectField = document.getElementById('accessories-select');
 var otherEventTypeTextField = document.getElementById('other-event-type');
 var eventTypeSelectField = document.getElementById('event-type-select');
 var eventTypePlaceholder = document.getElementById('event-type-placeholder');
-var resetLabel = document.getElementById('event-type-reset-label');
-var resetBox = document.getElementById('event-type-reset-box');
+var resetOtherEventLabel = document.getElementById('event-type-reset-label');
+var resetOtherEventBox = document.getElementById('event-type-reset-box');
+var resetOtherAccessoryLabel = document.getElementById('other-accessory-reset-label');
+var resetOtherAccessoryBox = document.getElementById('other-accessory-reset-box');
+var otherAccessoryTextLabel = document.getElementById('other-accessory-label');
+var otherAccessoryTextField = document.getElementById('other-accessory');
+let lastSelectedAccessory = [];
 
-if(clearAccessoriesCheckbox && accessoriesSelectField) {
+function hideOtherAccessoryElements() {
+    otherAccessoryTextLabel.style.display = 'none';
+    otherAccessoryTextField.style.display = 'none';
+    otherAccessoryTextField.value = '';
+}
+
+if(accessoriesSelectField && otherAccessoryTextLabel && otherAccessoryTextField) {
+    accessoriesSelectField.addEventListener('change', function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        const currentSelected = Array.from(event.target.selectedOptions).map(opt => opt.text);
+        const newlySelected = currentSelected.find(text => !lastSelectedAccessory.includes(text));
+        console.log(`current selected:  ${currentSelected}, newly selected: ${newlySelected}`)
+        if(newlySelected === 'Other') {
+            otherAccessoryTextLabel.style.display = 'block';
+            otherAccessoryTextField.style.display = 'block';
+            otherAccessoryTextField.focus();
+        } else if(!currentSelected.includes('Other')) {
+            hideOtherAccessoryElements();
+        }
+        lastSelectedAccessory = currentSelected;
+    });
+}
+
+if(clearAccessoriesCheckbox && accessoriesSelectField && otherAccessoryTextLabel && otherAccessoryTextField) {
     clearAccessoriesCheckbox.addEventListener('change', function(event) {
         event.preventDefault();
         event.stopPropagation();
         if (event.target.checked) {
             console.log('Checkbox is checked! ✅');
             accessoriesSelectField.selectedIndex = -1;
+            hideOtherAccessoryElements();
+            lastSelectedAccessory = [];
+            setTimeout(() => { event.target.checked = false; }, 2000);
         }
     });
 }
 
 if(accessoriesSelectField && clearAccessoriesCheckbox) {
-    accessoriesSelectField.addEventListener('click', function(event) {
+    accessoriesSelectField.addEventListener('change', function(event) {
         event.preventDefault();
         event.stopPropagation();
         clearAccessoriesCheckbox.checked = false;
     });
 }
 
-if(otherEventTypeTextField && eventTypeSelectField && resetBox && resetLabel) {
+if(otherEventTypeTextField && eventTypeSelectField && resetOtherEventBox && resetOtherEventLabel) {
     eventTypeSelectField.addEventListener('change', function(event) {
         event.preventDefault();
         event.stopPropagation();
@@ -35,19 +67,19 @@ if(otherEventTypeTextField && eventTypeSelectField && resetBox && resetLabel) {
             selectElement.style.display = 'none';
             otherEventTypeTextField.style.display = 'block';
             otherEventTypeTextField.focus();
-            resetLabel.style.display = 'block';
-            resetBox.style.display = 'block';
+            resetOtherEventLabel.style.display = 'block';
+            resetOtherEventBox.style.display = 'block';
         }
     });
 }
 
-if(resetBox && resetLabel && eventTypeSelectField) {
-    resetBox.addEventListener('click', function(event) {
+if(resetOtherEventBox && resetOtherEventLabel && eventTypeSelectField) {
+    resetOtherEventBox.addEventListener('click', function(event) {
         event.preventDefault();
         event.stopPropagation();
         if(event.target.checked) {
-            resetLabel.style.display = 'none';
-            resetBox.style.display = 'none';
+            resetOtherEventLabel.style.display = 'none';
+            resetOtherEventBox.style.display = 'none';
             eventTypeSelectField.style.display = 'block';
             otherEventTypeTextField.style.display = 'none';
             otherEventTypeTextField.value = '';
@@ -55,3 +87,4 @@ if(resetBox && resetLabel && eventTypeSelectField) {
         }
     });
 }
+
