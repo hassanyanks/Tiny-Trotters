a8c59d23f8d313d2cc760ce4eb5243a613011e9c
unknown <hassan.yanks@gmail.com>
2026-09-04 16:29:01 -0500

    adding rejectUnauthorized: false to Redis config trying to Redis to connect on Render

--- ponies/bin/redis.js
+++ ponies/bin/redis.js
@@ -2,7 +2,9 @@ import 'dotenv/config';
 import { createClient } from 'redis';
 
 const redisClient = createClient({
-    url: process.env.REDIS_URL || 'redis://127.0.0.1:6379'
+    url: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
+    tls: true,
+    rejectUnauthorized: false
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error:', err));
