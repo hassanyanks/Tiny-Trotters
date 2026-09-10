5c305ef00bbf4639bcf292f77378d23dbf52cbe0
unknown <hassan.yanks@gmail.com>
2026-09-10 16:50:32 -0500

    adding res.locals.isAuthenticated = req.isAuthenticated(); and res.locals.currentUser = req.user || null; to indexControllers.js trying to get Log Out to correctly render after a log in

--- ponies/controllers/indexControllers.js
+++ ponies/controllers/indexControllers.js
@@ -6,7 +6,9 @@ export const index = asyncHandler(async (req, res, next) => {
     res.locals.citiesServed = cachedCitiesStr; 
     const userRole = req.session.userRole;
     delete req.session.userRole;
-    return res.render("index", { userRole }); 
+    res.locals.isAuthenticated = req.isAuthenticated();
+    res.locals.currentUser = req.user || null; 
+    return res.render("index"); 
 
   } catch (error) { 
     return next(error); 
